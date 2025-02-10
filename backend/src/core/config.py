998bee5e34cb9b52b2e14d666cca918cfc190f2c
Corsiw <corsiw.dt.7@gmail.com>
2025-02-10 18:58:38 +0300

    Add .env support, Add postgres in docker-compose

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,16 +1,19 @@
+import os
+from pathlib import Path
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path(__file__).resolve().parents[2] / '.env'
+load_dotenv(dotenv_path=env_path)
 
 
 class Settings(BaseSettings):
 
-    postgres_user: str = "postgres"
-    postgres_password: str = "postgres"
-    postgres_host: str = "localhost"
-    postgres_port: str = "5432"
-    postgres_db: str = "postgres"
+    postgres_user: str = os.getenv("POSTGRES_USER", "postgres")
+    postgres_password: str = os.getenv("POSTGRES_PASSWORD", "postgres")
+    postgres_host: str = os.getenv("POSTGRES_HOST", "localhost")
+    postgres_port: str = os.getenv("POSTGRES_PORT", "5432")
+    postgres_db: str = os.getenv("POSTGRES_DB", "postgres")
 
     @property
     def database_url(self):
