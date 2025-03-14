f27677797bf33146068585e014247272f27575d4
Chadamik <den4ikbarsik@gmail.com>
2025-03-15 01:04:36 +0300

    Final Frontend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,26 +2,6 @@ import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 function Header() {
-  if (!localStorage.getItem("token"))
-  {
-    return (
-      <div className = "header">
-        <div className = "navigation">
-          <div className="navigation-list">
-            <div className="navigation-item"><Link to="/">Главная</Link></div>
-            <div className="navigation-item"><Link to="/schedule">Афиша</Link></div>
-            {/* <div className="navigation-item"><Link to="/about">О нас</Link></div>
-            <div className="navigation-item"><Link to="/contacts">Контакты</Link></div> */}
-            <div className="navigation-item"><Link to="/profile">Профиль</Link></div>
-            <div className="navigation-item"><Link to="/auth">Вход/Регистрация</Link></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  else
-  {
     return (
       <div className = "header">
         <div className = "navigation">
@@ -31,9 +11,7 @@ function Header() {
             <div className="navigation-item"><Link to="/profile">Профиль</Link></div>
           </div>
         </div>
-      </div>
-    );
-  }
+      </div>)
 }
 
 export default Header;
